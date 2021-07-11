704851dbc409bbb8491164e5e143eae7779870ef
Ilya.Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-07-11 14:18:47 +0300

    Implemented ProductController.

---
+++ AuthenticationService/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AuthenticationService.Filters;
+using AuthenticationServiceWithCustomJwt.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationServiceWithCustomJwt.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("v{version:apiVersion}/[controller]")]
+    [TokenAuthenticationFilter]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Product[] {
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "The first product"
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "The second product"
+                }
+            });
+        }
+    }
+}
